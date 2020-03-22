23d05d255ba3b1cfa89e49f05ac69ada3e13eba6
AmandaZhu360 <amanda.zhu@uwaterloo.ca>
2020-03-22 11:43:50 -0400

    added media queries for design and color windows

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
     var a = 110;
     $('.window').draggable({
-   		start: function(event, ui) { $(this).css("z-index", a++); }
+		start: function(event, ui) { $(this).css("z-index", a++); }
 	});	
     $('.window div').click(function() {
-        $(this).addClass('top').removeClass('bottom');
-        $(this).siblings().removeClass('top').addClass('bottom'); 
         $(this).css("z-index", a++);
 	});
 	
